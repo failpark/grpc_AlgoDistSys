04967882b6a999ca5cafb02e1d773bb2a0c74600
phedias <daniel.borgs@study.thws.de>
2025-10-17 11:14:04 +0200

    style: use tabs

--- client/client.js
+++ client/client.js
@@ -5,31 +5,31 @@ const path = require('path');
 const PROTO_PATH = path.join(__dirname, '../proto/fileservice.proto');
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true
 });
 
 const fileservice = grpc.loadPackageDefinition(packageDefinition).fileservice;
 
 function main() {
-  const client = new fileservice.FileService('localhost:50051', grpc.credentials.createInsecure());
+	const client = new fileservice.FileService('localhost:50051', grpc.credentials.createInsecure());
 
-  const directoryPath = process.argv[2] || '.';
+	const directoryPath = process.argv[2] || '.';
 
-  client.listDirectory({ path: directoryPath }, (error, response) => {
-    if (error) {
-      console.error('Error:', error.details);
-      return;
-    }
+	client.listDirectory({ path: directoryPath }, (error, response) => {
+		if (error) {
+			console.error('Error:', error.details);
+			return;
+		}
 
-    console.log(`Files in directory '${directoryPath}':`);
-    response.filenames.forEach(filename => {
-      console.log(`  ${filename}`);
-    });
-  });
+		console.log(`Files in directory '${directoryPath}':`);
+		response.filenames.forEach(filename => {
+			console.log(`	${filename}`);
+		});
+	});
 }
 
 main();
