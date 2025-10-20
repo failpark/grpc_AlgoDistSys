1cae24658e81f99d1517dbc76549ad4294037b1d
phedias <daniel.borgs@study.thws.de>
2025-10-20 17:39:46 +0200

    feat: add go client

---
+++ client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "grpc-seminar/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, _ := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	defer conn.Close()
+	c := pb.NewFileServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	resp, err := c.ListDir(ctx, &pb.Path{Path: "."})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, n := range resp.Filenames {
+		fmt.Println(n)
+	}
+}
--- justfile
+++ justfile
@@ -7,3 +7,9 @@ start:
 
 run *path:
 	npm run client "{{path}}"
+
+run-go:
+	go run client/main.go
+
+compile:
+	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --go-grpc_out=. proto/fileservice.proto
